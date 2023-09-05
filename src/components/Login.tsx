1b6829102678857272ba422e337a7e4e452e949b
iamipyana <tech.ipyana01@gmail.com>
2023-09-05 08:44:58 +0300

    Update Login.tsx

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -3,12 +3,32 @@ import { Fragment } from "react";
 function Login() {
   return (
     <Fragment>
-      <h4>CashFlow | Login</h4>
-      <form className="form">
-        <input type="text" className="form-control" />
-        <input type="text" className="form-control" />
-        <input type="submit" className="btn btn-primary" value="Login" />
-      </form>
+      <div className="card col-md-4">
+        <div className="card-header text-center">
+          <h4>CashFlow | Login</h4>
+        </div>
+        <div className="card-body">
+          <form className="form">
+            <div className="form-group">
+              <label>Username</label>
+              <input type="text" className="form-control" />
+            </div>
+            <div className="form-group">
+              <label>Password</label>
+              <input type="text" className="form-control" />
+            </div>
+            <button
+              type="submit"
+              className="btn btn-primary mt-2"
+              value="Login"
+            >
+              Login
+            </button>
+            Have no account?<a href="#">Register Here</a>
+          </form>
+        </div>
+        <div className="card-footer text-muted text-center">@2023</div>
+      </div>
     </Fragment>
   );
 }
